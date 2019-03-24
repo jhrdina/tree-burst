6716cfac5a53e532f26c9b80f73c18976c98bfd4
Jan Hrdina <jan.hrdka@gmail.com>
2019-03-24 22:20:55 +0100

    Register Service Worker only in production

--- src/TreeBurst.js
+++ src/TreeBurst.js
@@ -2,7 +2,7 @@
 import "./BootstrapNewMUIStyles";
 import "./TreeBurstApp.bs";
 
-if ("serviceWorker" in navigator) {
+if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/service-worker.js")
