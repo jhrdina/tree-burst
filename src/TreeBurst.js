7eae1d9b0400a50f94041ef8083454a69506b09a
Jan Hrdina <jan.hrdka@gmail.com>
2019-03-24 21:38:25 +0100

    Implement Service Worker

--- src/TreeBurst.js
+++ src/TreeBurst.js
@@ -1,3 +1,16 @@
 // This file is written directly in JS because we need exact control over imports order to ensure that the following module gets imported before everything else:
 import "./BootstrapNewMUIStyles";
 import "./TreeBurstApp.bs";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered.");
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
 const outputDir = path.join(__dirname, "build/");
 
 const isProd = process.env.NODE_ENV === "production";
@@ -15,6 +17,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "src/index.html",
       inject: false
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
     })
   ],
   devServer: {
